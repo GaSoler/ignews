9955f524aa7e91881adbb49336e8d7941fdb1d06
Gabriel Eduardo Soler da Angela <gabriel.angela@anchieta.br>
2022-08-25 13:01:32 -0300

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,10 +13,37 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn(user, account, profile) {
-      await fauna.query
+    async signIn({user, account, profile}) {
+      const { email } = user
 
-      return true
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ), 
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
     },
   }
 })
